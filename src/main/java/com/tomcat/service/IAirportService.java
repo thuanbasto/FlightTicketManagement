47a27b9fb7aa9d25f86f9ceb532d8748c69e07a2
hoagooner <tranhoa2289@gmail.com>
2021-02-08 22:27:54 +0700

    merge main

---
+++ src/main/java/com/tomcat/service/IAirportService.java
@@ -0,0 +1,16 @@
+package com.tomcat.service;
+
+import java.util.List;
+
+import com.tomcat.dto.AirportDTO;
+
+public interface IAirportService {
+
+	public List<AirportDTO> getList();
+
+	public AirportDTO get(String id);
+
+	public void save(AirportDTO airportDTO);
+
+	public void delete(String id);
+}
--- src/main/java/com/tomcat/service/impl/CityService.java
+++ src/main/java/com/tomcat/service/impl/CityService.java
@@ -1,12 +1,15 @@
 package com.tomcat.service.impl;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.tomcat.converter.CityConverter;
 import com.tomcat.dto.CityDTO;
+import com.tomcat.entity.City;
 import com.tomcat.repository.CityRepository;
 import com.tomcat.service.ICityService;
 
@@ -16,18 +19,37 @@ public class CityService implements ICityService{
 
 	@Autowired
 	CityRepository cityRepository;
-
-	@Override
-	public CityDTO getCityDTO(String id) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
+	
+	@Autowired
+	private CityConverter cityConverter;
+	
 	@Override
 	public List<CityDTO> getList() {
-		// TODO Auto-generated method stub
-		return null;
+		List<City> citys =  cityRepository.findAll();
+		List<CityDTO> cityDTOs = new ArrayList<>();
+		citys.forEach(city -> cityDTOs.add(cityConverter.toCityDTO(city)));
+		return cityDTOs;
 	}
-	
+
+	@Override
+	public void save(CityDTO cityDTO) {
+		City city = cityConverter.toCity(cityDTO);
+		cityRepository.save(city);
+	}
+
+
+	@Override
+	public void delete(String id) {
+		cityRepository.delete(id);
+	}
+
+	@Override
+	public CityDTO get(String id) {
+		City city = cityRepository.findOne(id);
+		if(city.getCity_Id() != null) {
+			return cityConverter.toCityDTO(city);
+		}
+		return new CityDTO();
+	}
 	
 }
